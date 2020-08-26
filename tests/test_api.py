80f5093d3ea8da8ed1be0d321577efa59ec4c38c
Mia Frank <31712634+miafrank@users.noreply.github.com>
2020-08-26 09:33:08 -0500

    added test for diet filter

--- tests/test_api.py
+++ tests/test_api.py
@@ -8,7 +8,7 @@ from api import *
 class TestApi(unittest.TestCase):
 
     def _mock_response(self,
-                       json_response,
+                       json,
                        status=HTTPStatus.OK,
                        raise_for_status=None):
         """
@@ -22,8 +22,8 @@ class TestApi(unittest.TestCase):
         # set status code
         mock_resp.status_code = status
         # include mock json response in Mock() return value
-        if json_response:
-            mock_resp.json = mock.Mock(return_value=json_response)
+        if json:
+            mock_resp.json = mock.Mock(return_value=json)
         return mock_resp
 
     @mock.patch('requests.get')
@@ -32,7 +32,7 @@ class TestApi(unittest.TestCase):
     # Otherwise, your code will not be able to use the Mock in place of the original object.
     def test_get_recipe_by_name(self, mock_get):
         expected_response = {'tests': 'meals'}
-        mock_response = self._mock_response(json_response=expected_response)
+        mock_response = self._mock_response(json=expected_response)
         mock_get.return_value = mock_response
 
         result = get_recipe_by_name(recipe_name='tests')
@@ -42,7 +42,7 @@ class TestApi(unittest.TestCase):
     @mock.patch('requests.get')
     def test_get_recipe_by_ingredients(self, mock_get):
         expected_response = {'the best': 'meal'}
-        mock_response = self._mock_response(json_response=expected_response)
+        mock_response = self._mock_response(json=expected_response)
         mock_get.return_value = mock_response
 
         result = get_recipe_by_ingredients(ingredients=['yummy', 'things'])
@@ -52,13 +52,23 @@ class TestApi(unittest.TestCase):
     @mock.patch('requests.get')
     def test_get_recipe_by_id(self, mock_get):
         expected_response = {'not': 'taco bell'}
-        mock_response = self._mock_response(json_response=expected_response)
+        mock_response = self._mock_response(json=expected_response)
         mock_get.return_value = mock_response
 
         result = get_recipe_by_id(recipe_id='8080')
 
         self.assertEqual(result, expected_response)
 
+    @mock.patch('requests.get')
+    def test_get_recipe_by_diet(self, mock_get):
+        expected_response = {'a very': 'vegan meal'}
+        mock_response = self._mock_response(json=expected_response)
+        mock_get.return_value = mock_response
 
+        result = get_recipe_by_diet(diet='vegan')
+
+        self.assertEqual(result, expected_response)
+
+g
 if __name__ == '__main__':
     unittest.main()
