6189e32a158d3c0784f20f251021f075bcae3c89
Mia Frank <31712634+miafrank@users.noreply.github.com>
2020-08-26 09:30:24 -0500

    added filter for diet

--- api.py
+++ api.py
@@ -10,13 +10,25 @@ def get_recipe_by_name(recipe_name: str, url=MEAL_DB_URL):
 
 
 def get_recipe_by_ingredients(ingredients: list):
-    ingredients_list = ','.join(ingredients)
-    resp = requests.get(f'{MEAL_DB_URL}/filter.php?i={ingredients_list}')
+    ingredients_query = ','.join(ingredients)
+    resp = requests.get(f'{MEAL_DB_URL}/filter.php?i={ingredients_query}')
     resp.raise_for_status()
     return resp.json()
 
 
 def get_recipe_by_id(recipe_id: str):
     resp = requests.get(f'{MEAL_DB_URL}/lookup.php?i={recipe_id}')
+    resp.raise_for_status()
+    return resp.json()
+
+
+def get_recipe_by_diet(diet=None):
+    resp = requests.get(f'{MEAL_DB_URL}/filter.php?c={diet}')
+    resp.raise_for_status()
+    return resp.json()
+
+
+def get_random_recipes():
+    resp = requests.get(f'{MEAL_DB_URL}/randomselection.php')
     resp.raise_for_status()
     return resp.json()
