8460ce83048497c7e6a0c4c1278faaf3748e3513
Mia Frank <31712634+miafrank@users.noreply.github.com>
2020-08-27 18:16:44 -0500

    removed react files, way too much work.

--- client/guac_is_extra/src/test.js
+++
@@ -1,6 +0,0 @@
-import APIClient from './client.js';
-
-const client = new APIClient();
-const call = client
-  .getRecipeByIngredient('chicken')
-  .then((data) => console.log(data['tacos']['results']));
